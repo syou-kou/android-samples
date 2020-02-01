ee8aafdff264b5dbdfe9e84bf33e23db89a5b3b8
syoukou <fatman01221087@gmail.com>
2020-02-01 14:56:31 +0900

    WikiSampleApp

--- WifiSampleApp/app/src/main/java/com/example/wifisampleapp/activities/MainActivity.java
+++ WifiSampleApp/app/src/main/java/com/example/wifisampleapp/activities/MainActivity.java
@@ -10,39 +10,56 @@ import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.wifisampleapp.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView mListView;
     private WifiManager wifiManager;
+    private BroadcastReceiver wifiScanReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context c, Intent intent) {
+            boolean success = intent.getBooleanExtra(
+                    WifiManager.EXTRA_RESULTS_UPDATED, false);
+            if (success) {
+                scanSuccess();
+            } else {
+                // scan failure handling
+                scanFailure();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        wifiManager = (WifiManager)getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        mListView = findViewById(R.id.wifi_list);
+        mListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1));
 
-        BroadcastReceiver wifiScanReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context c, Intent intent) {
-                boolean success = intent.getBooleanExtra(
-                        WifiManager.EXTRA_RESULTS_UPDATED, false);
-                if (success) {
-                    scanSuccess();
-                } else {
-                    // scan failure handling
-                    scanFailure();
-                }
-            }
-        };
+        wifiManager = (WifiManager)getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+    }
+
+    @Override
+    protected void onPause() {
+        unregisterReceiver(wifiScanReceiver);
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
 
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
-        getApplicationContext().registerReceiver(wifiScanReceiver, intentFilter);
+        registerReceiver(wifiScanReceiver, intentFilter);
 
         boolean success = wifiManager.startScan();
         if (!success) {
@@ -51,12 +68,31 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void displayList(List<String> dataList) {
+        ArrayAdapter adapter = (ArrayAdapter)mListView.getAdapter();
+        adapter.clear();
+        adapter.addAll(dataList);
+        adapter.notifyDataSetChanged();
+    }
+
     private void scanSuccess() {
         Log.d(MainActivity.class.getSimpleName(), "成功しました");
         List<ScanResult> results = wifiManager.getScanResults();
+        List<String> dataList = new ArrayList<>();
         for (ScanResult result : results) {
             Log.d(MainActivity.class.getSimpleName(), "結果:SSID/" + result.SSID);
+            String ssid = result.SSID;
+            if (!ssid.isEmpty()) {
+                dataList.add(ssid);
+            }
+            // タップしたタイミングで書く
+//            Bundle args = new Bundle();
+//            args.putParcelable("", result);
+//            Intent intent = new Intent();
+//            intent.putExtra("", args);
+//            startActivity(intent);
         }
+        displayList(dataList);
     }
 
     private void scanFailure() {
